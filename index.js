dadee84dafc154c17604490afefa35966cc5d0f9
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-05-29 09:21:01 -0700

    Implement sharp to resize image then re upload the image.

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const AWS = require("@aws-sdk");
+const sharp = require("sharp");
 
 exports.handler = async (event) => {
   // Read data from event object
-  const sourceRegion = event.Records[0].awsRegion;
-  const sourceBucket = event.Records[0].s3.bucket.name;
-  const sourceKey = event.Records[0].s3.object.sourceKey;
+  const region = event.Records[0].awsRegion;
+  const bucket = event.Records[0].s3.bucket.name;
+  const imageKey = event.Records[0].s3.object.sourceKey;
 
   // Instantiate a new s3 client
   const s3 = new AWS.S3({
-    region: sourceRegion
+    region: region
   });
 
   try {
-    const originalImage = await s3.getObject({Bucket: sourceBucket, Key: sourceKey}, (err, data) => {
-      if (err) console.log(err);
-      else 
-    })
+    // Return if image has already been resized
+    if (imageKey.includes("_resized")) {
+      console.log("The image has already been resized.");
+      return;
+    }
+
+    // Get the original image
+    const originalImage = await s3.getObject({Bucket: bucket, Key: imageKey}).promise();
+
+    // Resize the image
+    const resizedImage = await sharp(originalImage.data)
+      .resize(300)
+      .toBuffer();
+
+    // Upload resized image  
+    return s3.send(new putObject({
+      Body: resizedImage,
+      Bucket: bucket,
+      Key: imageKey.replace(".", "_resized")
+    }));
+
   } catch (error) {
-
+    console.log("There was an error:" + error);
+    res.send("There was an error: " + error);
   }
-
-  const copyObjectParams = {
-    Bucket: process.env.DEST_BUCKET,
-    Key: sourceKey,
-    CopySource: `${sourceBucket}/${sourceKey}`
-  };
-
-  return s3Client.send(new CopyObjectCommand(copyObjectParams));
 };
