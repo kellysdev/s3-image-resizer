d3853b78675b8766d6b9903bff56d2f965fc8484
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-05-30 14:34:06 -0700

    Rework logic and go back to using AWS SDK v2 because changed Lambda runtime to Node.js 16.x
    Set file type when getting image instead of using mime-types.

--- index.js
+++ index.js
@@ -1,54 +1,39 @@
-const S3Client = require("@aws-sdk/client-s3"),
-  sharp = require("sharp"),
-  mime = require("mime-types");
+const AWS = require("aws-sdk"),
+  sharp = require("sharp");
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   // Read data from event object
   const region = event.Records[0].awsRegion;
   const bucket = event.Records[0].s3.bucket.name;
   const imageKey = event.Records[0].s3.object.sourceKey;
   console.log(imageKey);
 
-  // Determine file type
-  const fileType = mime.lookup(imageKey);
-  console.log(fileType);
-
   // Instantiate a new s3 client
-  const s3 = new S3Client({
+  const s3 = new AWS.S3({
     region: region
   });
 
-  try {
-    // Return if image has already been resized
-    if (imageKey.includes("_resized")) {
-      console.log("The image has already been resized.");
-      return;
-    }
+  // Return if image has already been resized
+  if (imageKey.includes("_resized")) {
+    console.log("The image has already been resized.");
+    return;
+  };
 
-    // Get the original image
-    const originalImage = await s3.send(GetObjectCommand({
-      Bucket: bucket,
-      Key: imageKey,
-      ResponseContentType: fileType
-    }));
-    console.log(originalImage);
-    res.setHeader("Content-Type", fileType);
-    originalImage.Body.pipe(res);
-
+  // Get the original image
+  s3.getObject({Bucket: bucket, Key: imageKey}).promise()
     // Resize the image
-    const resizedImage = await sharp(originalImage)
+    .then(data => sharp(data.Body)
       .resize(300)
-      .toBuffer();
-
-    // Upload resized image 
-    return await s3.send(new PutObjectCommand({
-      Body: resizedImage,
-      Bucket: bucket,
-      Key: imageKey.replace(".", "_resized")
-    }));
-
-  } catch (error) {
-    console.log("There was an error:" + error);
-    res.status(500).send("There was an error: " + error);
-  }
+      .toFormat("png")
+      .toBuffer()
+    )
+    // Reupload the image
+    .then(buffer => s3.putObject({
+        Body: buffer,
+        Bucket: bucket,
+        Key: imageKey.replace(".", "_resized"),
+        ContentType: "image/png"
+      }).promise()
+    )
+    .catch(err => console.log(err));
 };
